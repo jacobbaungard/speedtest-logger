c63e7f864ccb99dfb367bd3a7c2013e4345ae1e5
Jacob Hansen <me@jacobbaungard.com>
2022-10-05 13:03:18 +0200

    Make copyright statement more generic
    To potentially avoid copyright notice confusions from potential
    contributors.
    
    Signed-off-by: Jacob Hansen <me@jacobbaungard.com>

--- internal/logger.go
+++ internal/logger.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 Jacob Baungard Hansen me@jacobbaungard.com
+Copyright © 2022 The speedtest-logger developers
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
